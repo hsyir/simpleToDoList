3b3859a268ff7c13acbc47c0a8c1971e28d1b7a9
Hossein Yaghmaee <hosseinyaghmaee@gmail.com>
2022-05-30 18:37:38 +0430

    Update loader.php

--- loader.php
+++ loader.php
@@ -3,12 +3,12 @@
 require(__DIR__ ."/functions.php");
 
     spl_autoload_register(function ($className) {
-    $filename = __DIR__ . DIRECTORY_SEPARATOR . $className . ".php";
-    $filename = str_replace('\\', DIRECTORY_SEPARATOR, $filename);
-    
-    if (file_exists($filename)) {
-        require $filename;
-    } else {
-        die($filename . " not exists!!");
-    }
-});
+        $filename = __DIR__ . DIRECTORY_SEPARATOR . $className . ".php";
+        $filename = str_replace('\\', DIRECTORY_SEPARATOR, $filename);
+        
+        if (file_exists($filename)) {
+            require $filename;
+        } else {
+            die($filename . " not exists!!");
+        }
+    });
